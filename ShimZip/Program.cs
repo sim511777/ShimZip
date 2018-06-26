d8b63f51211db2c8a0dbcc61524a281082efa136
sim51177@gmail.com <ssk1214>
2018-06-26 09:51:54 +0900

    명령줄 계획

--- ShimZip/Program.cs
+++ ShimZip/Program.cs
@@ -10,10 +10,15 @@ namespace ShimZip {
       /// 해당 응용 프로그램의 주 진입점입니다.
       /// </summary>
       [STAThread]
-      static void Main() {
+      static void Main(string[] args) {
          Application.EnableVisualStyles();
          Application.SetCompatibleTextRenderingDefault(false);
-         Application.Run(new FormMain());
+         if (args.Length == 0) {
+            Application.Run(new FormMain());
+         } else {
+            //CheckCommandFormatCorrect(args); // 명령줄 포멧이 맞는지 확인
+            //CheckCommandParamsValid(args);   // 파라미터가 유효한지 확인
+         }
       }
    }
 }
